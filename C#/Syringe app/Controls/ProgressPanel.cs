3fd7a42b3b286002b8756e606d3c40cff2cb6688
MKuj <73233657+MKuj@users.noreply.github.com>
2021-01-15 15:48:20 +0100

    Add files via upload

---
+++ C#/Syringe app/Controls/ProgressPanel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Syringe_app.Properties;
+using System.Resources;
+
+namespace Syringe_app
+{
+    public partial class ProgressPanel : UserControl
+    {
+        private VisualProperties.Colors colors = new VisualProperties.Colors();
+        private ResourceManager rm = Resources.ResourceManager;
+
+        private int progress_bar_complete = 0;
+
+        public ProgressPanel()
+        {
+            InitializeComponent();
+            initProperties();
+        }
+
+        private void initProperties()
+        {
+            run_status.Text = "";
+            time_elapsed_txt.Text = "";
+
+            time_elapsed_txt.ForeColor = colors.fore_color;
+            pumped.ForeColor = colors.fore_color;
+
+            syringe_pict.Image = Resources.Strzykawka_0000;
+
+            progress_bar.BackColor = colors.fore_color;
+            progress_bar_complete = progress_bar.Width;
+            progress_bar.Width = 0;
+
+            run_status.ForeColor = colors.fore_color;
+        }
+
+        public void setProgressValue(double value)
+        {
+            progress_bar.Width = progress_bar_complete * (int)value / 100;
+        }
+
+        public void setSyringeImage(double value)
+        {
+            if ((int)value <= 20 && (int)value >= 0)
+                syringe_pict.Image = (Bitmap)rm.GetObject(string.Format("Strzykawka_00{0}", (30 - (int)value)));
+            else if ((int)value > 20)
+                syringe_pict.Image = (Bitmap)rm.GetObject(string.Format("Strzykawka_000{0}", (30 - (int)value)));
+            else if ((int)value < 0)
+                syringe_pict.Image = (Bitmap)rm.GetObject(string.Format("Strzykawka_00{0}", 30));
+        }
+
+        public void setInjectedText(string text)
+        {
+            injected.Text = text;
+        }
+
+        public void setPumpedText(string text)
+        {
+            pumped.Text = text;
+    }
+
+        public void setPercentageStatusValue(double prc)
+        {
+            run_status.Text = String.Format("{0:0.00}%", prc);
+        }
+
+        public void setElapsedTime(TimeSpan ts)
+        {
+            time_elapsed_txt.Text = string.Format("Elapsed time: {0:D2}h:{1:D2}m:{2:D2}s:{3:D3}ms",
+                                            ts.Hours,
+                                            ts.Minutes,
+                                            ts.Seconds,
+                                            ts.Milliseconds
+                                            );
+        }
+    }
+}
---
+++ C#/Syringe app/IO/Commands.cs
@@ -0,0 +1,18 @@
+﻿namespace Syringe_app
+{
+    enum Commands
+    {
+        RUN = 10,
+        FORWARD = 20,
+        BACKWARD = 22,
+        HOME = 30,
+        SET_PERIOD = 80,
+        SET_START = 32,
+        STOP_MOVE = 54,
+        STOP_POS = 12,
+        BOLUS = 40,
+        HARD_STOP = 14,
+        CHANGE_STEP = 16,
+        READ_POS = 35
+    }
+}
